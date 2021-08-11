926730d7d278bc81c235b8d8ca0dd89b657f3843
Ida Olson <idaolson@gmail.com>
2021-08-11 09:31:17 -0600

    Update item spec for it. 2

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Item do
       expect(item1.name).to eq("Chalkware Piggy Bank")
     end
   end
+
+  context "iteration 2" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+
+    it "has a hash of bids" do
+      expect(item1.bids).to eq({})
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+    end
+  end
 end
