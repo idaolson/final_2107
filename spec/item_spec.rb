51be306c734368e891bf17bbb6598ee68b0834d6
Ida Olson <idaolson@gmail.com>
2021-08-11 09:42:47 -0600

    Finish it. 2 in item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -29,5 +29,9 @@ RSpec.describe Item do
       item1.add_bid(attendee1, 22)
       expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
     end
+
+    it "determines current high bid" do
+      expect(item1.current_high_bid).to eq(22)
+    end
   end
 end
