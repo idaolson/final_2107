860ebcd246c37eb3d35171c3200b354f840be7c9
Ida Olson <idaolson@gmail.com>
2021-08-11 10:36:31 -0600

    Finish it. 3 spec

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -54,4 +54,38 @@ RSpec.describe Auction do
       expect(auction.potential_revenue).to eq(87)
     end
   end
+
+  context "iteration 3" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    it "returns list of bidders by name" do
+      expect(auction.bidders).to eq(["Megan", "Bob", "Mike"])
+    end
+
+    it "retruns a hash of attendees and their budgets and what they've bid on" do
+      result = {
+          attendee1 => {:budget => 50, :items => [item1]},
+          attendee2 => {:budget => 75, :items => [item1, item3]},
+          attendee3 => {:budget => 100, :items => [item4]}
+         }
+      expect(auction.bidder_info).to eq(result)
+    end
+  end
 end
