e7687e68a4e4c1c2636b659136377a1db3131e24
Ida Olson <idaolson@gmail.com>
2021-08-11 09:50:26 -0600

    Finish it. 2 in auction class

--- lib/auction.rb
+++ lib/auction.rb
@@ -12,4 +12,20 @@ class Auction
   def item_names
     @items.map { |item| item.name }
   end
+
+  def unpopular_items
+    @items.find_all do |item|
+      item.bids == {}
+    end
+  end
+
+  def potential_revenue
+    @items.sum do |item|
+      if item.current_high_bid == nil
+        0
+      else
+        item.current_high_bid
+      end
+    end
+  end
 end
