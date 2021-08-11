60ce8f62e19d0ca4291964c6bf417d3ca28b5acb
Ida Olson <idaolson@gmail.com>
2021-08-11 10:02:28 -0600

    Update for it. 3

--- lib/item.rb
+++ lib/item.rb
@@ -1,16 +1,23 @@
 class Item
-  attr_reader :name, :bids
+  attr_reader :name, :bids, :bidding_open
 
   def initialize(name)
     @name = name
     @bids = {}
+    @bidding_open = true
   end
 
   def add_bid(attendee, bid)
-    @bids[attendee] = bid
+    if @bidding_open
+      @bids[attendee] = bid
+    end
   end
 
   def current_high_bid
     @bids.values.max
+  end
+
+  def close_bidding
+    @bidding_open = false
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Item do
     it "exists and has attributes" do
       expect(item1).to be_an(Item)
       expect(item1.name).to eq("Chalkware Piggy Bank")
+      expect(item1.bidding_open).to be(true)
     end
   end
 
@@ -32,6 +33,23 @@ RSpec.describe Item do
 
     it "determines current high bid" do
       expect(item1.current_high_bid).to eq(22)
+    end
+  end
+
+  context "iteration 3" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    it "closes bidding" do
+      expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+      item1.close_bidding
+      item1.add_bid(attendee3, 70)
+      expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
     end
   end
 end
